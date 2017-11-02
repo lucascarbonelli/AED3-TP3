2592f435acf7a3a3f84f096769923ed7924d01af
istng <ivanstng@gmail.com>
2017-11-02 14:17:43 -0300

    falta arreglar save_pop y ver bien fitness

--- genetic.cpp
+++ genetic.cpp
@@ -20,14 +20,14 @@ void init_population(vector<individual>& population){
 
 
 
-void selection_helix(int n, int m , int c, int p, bool red_player_first, vector<individual>& population, vector<individual>& new_population, int news, int breeds, string fitness, int quantInd_a_Cross){
+void selection_helix(int n, int m , int c, int p, bool red_player_first, vector<individual>& population, vector<individual>& new_population, int news, int breeds, string fitness, int iter, int quantInd_a_Cross){
 
   for (int i = 0; i < news; ++i){
     new_population.push_back(population[population.size()-1-i]);
   }
 
   vector<individual> better_ones(breeds);
-  get_better_ones(n, m , c, p, red_player_first, better_ones, population, fitness);
+  get_better_ones(n, m , c, p, red_player_first, iter, better_ones, population, fitness);
 
   vector<individual> old_population;
   for (int i = 0; i < population.size()-news; ++i){
@@ -35,7 +35,7 @@ void selection_helix(int n, int m , int c, int p, bool red_player_first, vector<
   }
 
   vector<individual> old_better_ones(breeds);
-  get_better_ones(n, m , c, p, red_player_first, old_better_ones, old_population, fitness);
+  get_better_ones(n, m , c, p, red_player_first, iter, old_better_ones, old_population, fitness);
 
 
   old_better_ones = crossover_twopops(old_better_ones, better_ones, quantInd_a_Cross);
@@ -46,18 +46,18 @@ void selection_helix(int n, int m , int c, int p, bool red_player_first, vector<
 
 }
 
-
-void get_better_ones(int n, int m , int c, int p, bool red_player_first, vector<individual>& better_ones, vector<individual>& population, string fitness){
+//copia a better_ones los better_ones.size() mejores según fitness (notar que hay que pasar vector inicializado con tamaño)
+void get_better_ones(int n, int m , int c, int p, bool red_player_first, int iter, vector<individual>& better_ones, vector<individual>& population, string fitness){
 
   //first es score y second es indice del individuo en population
   vector<pair<int, unsigned int> > scores(population.size());
 
   for (int i = 0; i < population.size(); ++i){
     if(fitness == "himself"){
-      scores[i].first = fitness_himself(n, m , c, p, red_player_first, population[i]);
+      scores[i].first = fitness_himself(n, m , c, p, red_player_first, population[i], iter);
     }
     if(fitness == "others"){
-      scores[i].first = fitness_others(n, m , c, p, red_player_first, population[i]);
+      scores[i].first = fitness_others(n, m , c, p, red_player_first, population[i], iter);
     }
     scores[i].second = i;
   }
@@ -105,13 +105,63 @@ individual breed(individual& individual_a, individual& individual_b, int quantIn
 }
 
 //se calcula fitness contra otras versiones de si mismo
-int fitness_himself(int n, int m , int c, int p, bool red_player_first, individual& population){
-  return 0;
+int fitness_himself(int n, int m , int c, int p, bool red_player_first, individual& individual_a, int iter){
+    /* Seteamos parametros */
+  string cmd = "python c_linea.py --blue_player ./genetic_player --red_player ./parametric_player";
+  cmd += " " + to_string(iter); /* cantidad de iteraciones */
+  cmd += " " + to_string(individual_a.size()); /* cantidad de parametros */
+
+  for (int i = 0; i < individual_a.size(); ++i){
+    cmd += " " + to_string(individual_a[i]);
+  }
+
+  cmd += " --red_player ./parametric_player";
+  cmd += " --iterations " + to_string(iter);
+  cmd += " --first azul --columns "+to_string(m)+" --rows "+to_string(n)+" --p "+to_string(p)+" --c "+to_string(c)+" ";
+
+  /* Corremos el juego que dejara los resultados en res.txt*/
+  system(cmd.c_str());
+  ifstream res("res.txt");
+  int won,lost,tied, mean_l_time, mean_w_time;
+  res >> won;
+  res >> lost;
+  res >> tied;
+  res >> mean_l_time;
+  res >> mean_w_time;
+  cout << "Ganados: "<< won << " | " << "Perdidos: " << lost << " | " << "Empatados: " << tied <<endl;
+  //cout << "Ratio:" << won/(iter)<< endl;;
+
+  float score = won*10/mean_w_time - lost/mean_l_time;
 }
 
 //se calcula fitness contra otros tipos de jugador
-int fitness_others(int n, int m , int c, int p, bool red_player_first, individual& population){
-  return 0; 
+int fitness_others(int n, int m , int c, int p, bool red_player_first, individual& individual_a, int iter){
+    /* Seteamos parametros */
+  string cmd = "python c_linea.py --blue_player ./minimax --red_player ./parametric_player";
+  cmd += " " + to_string(iter); /* cantidad de iteraciones */
+  cmd += " " + to_string(individual_a.size()); /* cantidad de parametros */
+
+  for (int i = 0; i < individual_a.size(); ++i){
+    cmd += " " + to_string(individual_a[i]);
+  }
+
+  cmd += " --red_player ./parametric_player";
+  cmd += " --iterations " + to_string(iter);
+  cmd += " --first azul --columns "+to_string(m)+" --rows "+to_string(n)+" --p "+to_string(p)+" --c "+to_string(c)+" ";
+
+  /* Corremos el juego que dejara los resultados en res.txt*/
+  system(cmd.c_str());
+  ifstream res("res.txt");
+  int won,lost,tied, mean_l_time, mean_w_time;
+  res >> won;
+  res >> lost;
+  res >> tied;
+  res >> mean_l_time;
+  res >> mean_w_time;
+  cout << "Ganados: "<< won << " | " << "Perdidos: " << lost << " | " << "Empatados: " << tied <<endl;
+  //cout << "Ratio:" << won/(iter)<< endl;;
+
+  float score = won*10/mean_w_time - lost/mean_l_time;
 }
 
 
@@ -131,7 +181,7 @@ void mutation(vector<individual>& population, int prob, int max){
 }
 
 /*---------------------------------------Auxiliares------------------------------------------*/
-
+/*
 
 //Formato de salida:
 //               T·.·_C·_P·/_I·_F·
@@ -153,7 +203,7 @@ void save_population(vector<individual> population, int n, int m, int c, int p){
         std::cout << "cannot create sessionnamefolder error:" << strerror(errno) << std::endl;
         throw std::runtime_exception( strerror(errno) );
       }
-    }*/
+    }//
   }
 
   string f_name = "training/"+finalpath+"/"+"_I"+to_string(population.size())+"_F"+to_string(population[0].size());
@@ -165,7 +215,7 @@ void save_population(vector<individual> population, int n, int m, int c, int p){
     }
     f_name << endl;
   }
-}
+}*/
 
 
 bool pairCompare(const pair<int, unsigned int>& firstElem, const pair<int, unsigned int>& secondElem){
--- genetic.h
+++ genetic.h
@@ -14,15 +14,15 @@ typedef vector<int> individual;
 
 void init_rnd_population(vector<individual>& population, unsigned int max);
 void init_population(vector<individual>& population);
-void selection_helix(int n, int m , int c, int p, bool red_player_first, vector<individual>& population, vector<individual>& new_population, int news, int breeds, string fitness, int quantInd_a_Cross);
-void get_better_ones(int n, int m , int c, int p, bool red_player_first, vector<individual>& better_ones, vector<individual>& population, string fitness);
+void selection_helix(int n, int m , int c, int p, bool red_player_first, vector<individual>& population, vector<individual>& new_population, int news, int breeds, string fitness, int iter, int quantInd_a_Cross);
+void get_better_ones(int n, int m , int c, int p, bool red_player_first, int iter, vector<individual>& better_ones, vector<individual>& population, string fitness);
 vector<individual> crossover_twopops(vector<individual>& population_a, vector<individual>& population_b, int quantInd_a);
 individual breed(individual& individual_a, individual& individual_b, int quantInd_a);
-int fitness_himself(int n, int m , int c, int p, bool red_player_first, individual& population);
-int fitness_others(int n, int m , int c, int p, bool red_player_first, individual& population);
+int fitness_himself(int n, int m , int c, int p, bool red_player_first, individual& population, int iter);
+int fitness_others(int n, int m , int c, int p, bool red_player_first, individual& population, int iter);
 void mutation(vector<individual>& population, int prob);
 
 //Auxiliares
 
-void save_population(vector<individual> population, int n, int m, int c, int p);
+//void save_population(vector<individual> population, int n, int m, int c, int p);
 bool pairCompare(const pair<int, unsigned int>& firstElem, const pair<int, unsigned int>& secondElem);
