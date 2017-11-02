92a06da2cdbc8ea6adc7e9457faa874628b7ff0d
juan <juanmanuelbaldonado@gmail.com>
2017-11-01 22:46:55 -0300

    gridsearch: backtracking y randomSearch

--- optimizer.cpp
+++ optimizer.cpp
@@ -1,15 +1,27 @@
 #include <stdlib.h>
 #include <iostream>
 #include <fstream>
+#include <vector>
 using namespace std;
 
-int score(/*vector<int> weights,*/ int iter){
+
+struct Solution{
+  vector<int> weights;
+  int score;
+};
+
+
+int fitness(vector<int> weights, int iter){
 
 	/* Seteamos parametros */
-	string cmd = "python c_linea.py --blue_player ./random_player --red_player ./parametric_player --first azul";
-	//for (int i = 0; i < weights.size(); ++i){
-	// 	cmd += " " + to_string(weights[i]);
-	//}
+	string cmd = "python c_linea.py --blue_player ./random_player --red_player ./parametric_player";
+  cmd += " " + to_string(iter); /* cantidad de iteraciones */
+  cmd += " " + to_string(weights.size()); /* cantidad de parametros */
+
+  for (int i = 0; i < weights.size(); ++i){
+	 	cmd += " " + to_string(weights[i]);
+	}
+
 	cmd += " --iterations " + to_string(iter);
 	cmd += " --columns 7 --rows 6 --p 21 --c 4";
 
@@ -21,13 +33,240 @@ int score(/*vector<int> weights,*/ int iter){
 	res >> lost;
 	res >> tied;
 	cout << "Ganados: "<< won << " | " << "Perdidos: " << lost << " | " << "Empatados: " << tied <<endl;
+  //cout << "Ratio:" << won/(iter)<< endl;;
 	return won;
+}
+
+void backtrack(vector<int>& weights, vector<int>& range, int n, Solution& S,int& iter){
+
+  /* caso base */
+  if(n == 0){
+    int score = fitness(weights,50);
+    if(score > S.score ){
+      //cout << "Found better score: " << score << endl;
+      S.weights = weights;
+      S.score = score;
+    }
+    /* para la ansiedad */
+    cout << "Iter: " << iter << endl;
+    iter++;
+    return;
+  }
+
+  for (size_t i = 0; i < range.size(); i++) {
+    weights.push_back(range[i]);
+    backtrack(weights,range,n-1,S,iter);
+    weights.pop_back();
+  }
+}
+
+vector<int> gridSearch(int params, vector<int> range){
+  int iter = 0;
+  vector<int> weights;
+  Solution S;
+  S.weights = weights;
+  S.score = -1;
+
+  backtrack(weights,range,params,S,iter);
+  return S.weights;
 
 }
+
+
+
+/********************************************************************/
+/********************************************************************/
+/********************************************************************/
+/********************************************************************/
+
+int compareWeights(vector<int> weights1,vector<int> weights2, int iter){
+
+	/* Seteamos parametros */
+	string cmd = "python c_linea.py --blue_player ./parametric_player";
+  /* pasamos weights 1 */
+  cmd += " " + to_string(iter); /* cantidad de iteraciones */
+  cmd += " " + to_string(weights1.size()); /* cantidad de parametros */
+
+  for (int i = 0; i < weights1.size(); ++i){
+    cmd += " " + to_string(weights1[i]);
+  }
+
+  /* pasamos weights 2*/
+  cmd+= " --red_player ./parametric_player";
+  cmd += " " + to_string(iter); /* cantidad de iteraciones */
+  cmd += " " + to_string(weights2.size()); /* cantidad de parametros */
+
+  for (int i = 0; i < weights2.size(); ++i){
+	 	cmd += " " + to_string(weights2[i]);
+	}
+
+	cmd += " --iterations " + to_string(iter);
+	cmd += " --first azul --columns 7 --rows 6 --p 21 --c 4";
+
+	/* Corremos el juego que dejara los resultados en res.txt*/
+	system(cmd.c_str());
+	ifstream res("res.txt");
+	int won,lost,tied;
+	res >> won;
+	res >> lost;
+	res >> tied;
+	//cout << "Ganados: "<< won << " | " << "Perdidos: " << lost << " | " << "Empatados: " << tied <<endl;
+
+  /* veamos quien es el mejor */
+  if(won + tied > lost) return 1;
+  return 0;
+
+}
+
+
+
+
+vector<int> generateRandomWeights(int params,vector<int> range){
+
+  vector<int> weights;
+  for (size_t i = 0; i < params; i++) {
+    int e = range[rand() % range.size()];
+    weights.push_back(e);
+  }
+  return weights;
+}
+
+
+
+vector<int> randomSearch(int params, vector<int> range, int iterations, vector<int>& init_Weights){
+
+  vector<int> weights(params,0);
+  vector<int> best_solution = init_Weights;
+
+  for (size_t i = 0; i < iterations; i++) {
+    cout << "Iteration: " << i << endl;
+    /* generamos una solucion al azar */
+    vector<int> sol = generateRandomWeights(params,range);
+    /* comaparamos con la mejor hasta el momento */
+    if(compareWeights(sol,best_solution,50) == 1){
+        cout << "Found better solution" << endl;
+        best_solution = sol;
+    }
+
+  }
+  return best_solution;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 int main(){
 
-	int won = score(100);
+  int c = 4;
+  int columns = 7;
+  vector<int> weights(c +columns+1,0);
+
+  for (int i = 0; i < c; i++) {
+    weights[i] = i;
+  }
+  for (int i = c ; i < c + columns; i++) {
+    weights[i] = -5*i;
+  }
+
+  vector<int> range{-1000, -100, -50, -10, 0, 10, 50, 100, 1000};
+  weights = randomSearch(c+columns+1,range,50,weights);
+
+  cout << "Weights: " << endl;
+  for (size_t i = 0; i < weights.size(); i++) {
+    cout << weights[i] << " ";
+  }
+  cout << endl;
+
+  int won = fitness(weights,1000);
+
 	return 0;
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//
+//int main(){
+//
+//  int c = 4;
+//  int columns = 7;
+//
+//  vector<int> weights(c +columns+1,0);
+//
+//  for (int i = 0; i < c; i++) {
+//    weights[i] = i;
+//  }
+//  for (int i = c ; i < c + columns; i++) {
+//    weights[i] = -5*i;
+//  }
+//
+//  int best_i,best_j;
+//  int best_score;
+//
+//  weights[c-1] = 10*7;
+//  weights[c+columns -1] = 10*4;
+//
+//  weights[c+columns] = 20000000;
+//
+//  vector<int> range{0,10,50,100,1000};
+//  weights = gridSearch(c+columns+1,range);
+//
+//  cout << "Weights: " << endl;
+//  for (size_t i = 0; i < weights.size(); i++) {
+//    cout << weights[i] << " ";
+//  }
+//  cout << endl;
+//
+//  //for (size_t i = 0; i < 10; i++) {
+//  //  for (size_t j = 0; j < 10; j++) {
+//
+//  //    cout << "Iteration: " << 10*i +j << endl;
+//
+//  //    weights[c-1] = 10*i;
+//  //    weights[c+columns -1] = 10*j;
+//  //    int won = score(weights,100);
+//  //    if(won > best_score){
+//  //      best_score = won;
+//  //      best_i = i;
+//  //      best_j = j;
+//  //    }
+//
+//
+//  //  }
+//  //}
+//
+//  //for (size_t i = 0; i < count; i++) {
+//  //  /* code */
+//  //}
+//
+//  int won = fitness(weights,1000);
+//
+//	return 0;
+//
+//}
+//
--- parametricPlayer.cpp
+++ parametricPlayer.cpp
@@ -15,7 +15,7 @@ using namespace std;
 
 
 /* Para jugar contra parametric_player :
->> python c_linea.py --blue_player ./parametric_player --first azul --ui True --columns 7 --rows 6 --p 21 --c 4
+>> python c_linea.py --blue_player ./parametric_player 10 12 -1000 -100 10 100 -50 -10 -10 -50 -100 -100 -1000 -1000 --first azul --ui True --columns 7 --rows 6 --p 21 --c 4
 */
 
 
@@ -55,7 +55,7 @@ std::string read_str() {
 /* Template de la funcion  */
 
 
-int scoreBoard(Board &b, int player, vector<int>& weights, ofstream& log){
+int scoreBoard(Board &b, int player, vector<int>& weights/*, ofstream& log*/){
 
   size_t feature = 0;
   int score = 0;
@@ -63,22 +63,16 @@ int scoreBoard(Board &b, int player, vector<int>& weights, ofstream& log){
   /* Lineas del jugador */
   vector<int> player_lines = b.lineCounts(PLAYER);
   for (size_t i = 0; i < player_lines.size(); i++) {
-    log << "Lineas de longitud " << i <<": "<< player_lines[i] << endl;
+    //log << "Lineas de longitud " << i <<": "<< player_lines[i] << endl;
     score += weights[feature] * player_lines[i];
     feature++;
   }
 
   /* Perfiles  */
-  vector<int> player_profile = b.player_prof(PLAYER);
-  for (size_t i = 0; i < player_profile.size(); i++) {
-    log << "si muevo a "<< i <<" "<< "puedo hacer una linea de longitud "  << player_profile[i] << endl;
-    //score += weights[feature] * player_profile[i];
-    feature++;
-  }
 
   vector<int> opponent_profile = b.player_prof(OPPONENT);
   for (size_t i = 0; i < opponent_profile.size(); i++) {
-    log << "si el mueve a "<< i <<" "<< "puede hacer una linea de longitud "  << opponent_profile[i] << endl;
+    //log << "si el mueve a "<< i <<" "<< "puede hacer una linea de longitud "  << opponent_profile[i] << endl;
     score += weights[feature] * opponent_profile[i];
     feature++;
   }
@@ -93,7 +87,7 @@ int scoreBoard(Board &b, int player, vector<int>& weights, ofstream& log){
 
 
 
-int generateAndScore(Board &b, int player,int c, vector<int>& weights, ofstream& log ){
+int generateAndScore(Board &b, int player,int c, vector<int>& weights/*, ofstream& log*/ ){
 
   int best_move = 0;
   int best_move_score = -INFINITY;
@@ -102,9 +96,9 @@ int generateAndScore(Board &b, int player,int c, vector<int>& weights, ofstream&
     if( !b.isColFull(col) ){
       /* Miramos el tablero al agregar en la columna col */
       b.addPlayer(col);
-      int score = scoreBoard(b,player,weights,log);
+      int score = scoreBoard(b,player,weights);
 
-      log << "move: " << col << " score: " << score <<endl;
+      //log << "move: " << col << " score: " << score <<endl;
       if(score > best_move_score){
         best_move = col;
         best_move_score = score;
@@ -113,21 +107,37 @@ int generateAndScore(Board &b, int player,int c, vector<int>& weights, ofstream&
       b.backtrack(col);
     }
   }
-  log << "Chose " << best_move << " with score " << best_move_score<< endl;
+  //log << "Chose " << best_move << " with score " << best_move_score<< endl;
   return best_move;
 }
 
 
 
-int main() {
+int main(int argc, const char* argv[]) {
+
+    int iters;
+    if(argc >= 2){
+      iters = atoi(argv[1]);
+    }
+
+    vector<int> weights;
+    if(argc >= 3){
+      int params = atoi(argv[2]);
+      for (size_t i = 0; i < params; i++) {
+        weights.push_back(atoi(argv[3+i]));
+      }
+    }
 
     std::string msg, color, oponent_color, go_first;
     int columns, rows, c, p, move;
 
-    /* Guardamos un log de las decision en log.txt */
-    ofstream log("log.txt");
-    int won = 0, lost = 0, tied = 0;
 
+    /* Guardamos un log de las decision en log.txt */
+    //ofstream log("log.txt");
+    ofstream res("res.txt");
+    //log << iters << endl;
+    int won = 0, lost = 0, tied = 0;
+    bool imBlue = false;
 
     while (true) {
         color = read_str();
@@ -139,27 +149,14 @@ int main() {
         p = read_int();
 
         Board board(columns, rows, c, 2*p); // 2*p fichas en total
-        /* Inicializamos los pesos */
-        vector<int> weights(c + 2*columns+1,0);
-
-        for (int i = 0; i < c - 1; i++) {
-          weights[i] = i;
-        }
-        for (int i = c; i < c + columns; i++) {
-          weights[i] = exp(i-c+1);
-        }
-        for (int i = c + columns; i < c + 2*columns ; i++) {
-          weights[i] = -5*i;
-        }
-
-        weights[c+2*columns] = INFINITY;
 
         // Primer movimiento
         go_first = read_str();
         if (go_first == "vos"){
-          log << "started \n" << endl;
-          move = generateAndScore(board,PLAYER,c,weights,log);
-          log << "move \n" << move <<  endl;
+          imBlue = true;
+          //log << "started \n" << endl;
+          move = generateAndScore(board,PLAYER,c,weights);
+          //log << "move \n" << move <<  endl;
           board.addPlayer(move);
           send(move);
         }
@@ -173,18 +170,17 @@ int main() {
                 break;
 
             }
-            log << "mi turno " << endl;
+            //log << "mi turno " << endl;
             board.addOpponent(std::stoi(msg));
-            move = generateAndScore(board,PLAYER,c,weights,log);
-            log << "move \n" << move <<  endl;
+            move = generateAndScore(board,PLAYER,c,weights);
+            //log << "move \n" << move <<  endl;
             board.addPlayer(move);
             send(move);
         }
-        //res << "Ganados: "<< won << " | " << "Perdidos: " << lost << " | " << "Empatados: " << tied <<endl;
-        ofstream res("res.txt");
-        res << won << endl << lost << endl << tied << endl;
+        iters--;
+        if (iters == 0 && imBlue) res << won << endl << lost << endl << tied << endl;
     }
-    //
+
 
     return 0;
 }
